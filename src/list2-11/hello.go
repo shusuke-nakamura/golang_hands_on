1c9f33798433a54510ccafa5c5c2c11afb874af4
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-07-08 08:49:16 +0900

    list2-13まで学習完了

---
+++ src/list2-11/hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"hello"
+	"strconv"
+)
+
+func main() {
+	x := hello.Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print("1から" + x + "の合計は、")
+	} else {
+		return
+	}
+
+	t := 0
+	for i := 0; i <= n; i++ {
+		t += i
+	}
+	fmt.Println(t, "です。")
+}
