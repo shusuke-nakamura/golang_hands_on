24e8141afc4975c5323425bd15699b3c06ef99ba
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-07-12 23:07:28 +0900

    list3-9まで学習完了

---
+++ src/chap03/list3-3/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 123
+	p := &n
+	q := &p
+	m := 1000
+	p2 := &m
+	q2 := &p2
+	fmt.Printf("q value:%d, address:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("q value:%d, address:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
+
+}
