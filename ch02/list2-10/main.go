366011ec2b6bd98bfa11caab5591791c98d10cb7
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-07 09:03:38 +0900

    list2-11まで学習完了

---
+++ ch02/list2-10/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"hello"
+	"strconv"
+)
+
+func main() {
+	x := hello.Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print("1から" + x + "の合計は、")
+	} else {
+		return
+	}
+
+	t := 0
+	c := 1
+	for c <= n {
+		t += c
+		c++
+	}
+
+	fmt.Println(t, "です。")
+}
---
+++ ch02/list2-11/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"hello"
+	"strconv"
+)
+
+func main() {
+	x := hello.Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print("1から", x+"の合計は、")
+	} else {
+		return
+	}
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	fmt.Println(t, "です。")
+}
