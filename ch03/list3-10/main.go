eeb1bab8724b0dc2939ebc795343b62123b29f92
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-08 16:27:55 +0900

    list3-13まで学習完了

---
+++ ch03/list3-10/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Mydata struct {
+	Name string
+	Data []int
+}
+
+func main() {
+	taro := new(Mydata)
+	fmt.Println(taro)
+	taro.Name = "Taro"
+	taro.Data = make([]int, 5, 5)
+	fmt.Println(taro)
+}
---
+++ ch03/list3-11/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Mydata is structure.
+type Mydata struct {
+	Name string
+	Data []int
+}
+
+// PrintData is println all data.
+func (md Mydata) PrintData() {
+	fmt.Println("*** Mydata ***")
+	fmt.Println("Name: ", md.Name)
+	fmt.Println("Data: ", md.Data)
+	fmt.Println("*** end ***")
+}
+
+func main() {
+	taro := Mydata{
+		"Hanako", []int{98, 76, 54, 32, 10},
+	}
+	taro.PrintData()
+}
