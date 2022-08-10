d57ab7f380a15a9d2682d1a3a4776c0523d01c45
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-10 20:32:53 +0900

    list4-15の学習完了

---
+++ ch04/list4-15-3/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	w.SetContent(
+		// NewFixedGridLayout → NewGridWrap
+		container.NewGridWrap(fyne.NewSize(200.0, 60.0),
+			widget.NewButton("One", nil),
+			widget.NewButton("Two", nil),
+			widget.NewButton("Three", nil),
+			widget.NewButton("Four", nil),
+			layout.NewSpacer(),
+			widget.NewButton("Five", nil),
+			widget.NewButton("Six", nil),
+			layout.NewSpacer(),
+			widget.NewButton("Seven", nil),
+			widget.NewButton("Eight", nil),
+			widget.NewButton("Nine", nil),
+			widget.NewButton("Ten", nil),
+		),
+	)
+	w.ShowAndRun()
+}
