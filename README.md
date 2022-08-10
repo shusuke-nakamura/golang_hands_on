d1a2f0e884cd4f363534d16799b1e665b5fe8e01
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-10 16:32:25 +0900

    list4-11まで学習完了

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 ## Fyne
 https://developer.fyne.io/
+【前提】
+・「gwsl」がインストール済みであること
+・ 以下のパッケージをインストールすること
+sudo apt install x11-apps
+sudo apt install libxcursor-dev
+sudo apt install libxrandr-dev
+sudo apt install libxinerama-dev
+sudo apt install libxi-dev
+sudo apt install libglx-dev
+sudo apt install libgl-dev
+sudo apt install libxxf86vm-dev
+
 ### 1. インストール方法
 コマンド: go get fyne.io/fyne/v2
 ### 2. 開発方法
---
+++ ch04/list4-11/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	l := widget.NewLabel("Hello Fyne!")
+	ne := widget.NewEntry()
+	pe := widget.NewPasswordEntry()
+
+	w.SetContent(
+		container.NewVBox(
+			l,
+			widget.NewForm(
+				widget.NewFormItem("Name", ne),
+				widget.NewFormItem("Pass", pe),
+			),
+			widget.NewButton("OK", func() {
+				l.SetText(ne.Text + " & " + pe.Text)
+			}),
+		),
+	)
+
+	w.ShowAndRun()
+}
