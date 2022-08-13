a4eebc8e168eac2702a0a093829c230da61db9d5
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-13 16:48:12 +0900

    list5-24まで学習完了

---
+++ ch05/list5-24/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.19
+
+require hello v0.0.0
+
+require github.com/mattn/go-sqlite3 v1.14.14
+
+replace hello => ../../hello
---
+++ ch05/list5-24/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"hello"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Mydata struct {
+	ID   int
+	Name string
+	Mail string
+	Age  int
+}
+
+func (m *Mydata) Str() string {
+	return "<\"" + strconv.Itoa(m.ID) + ":" + m.Name + "\" " + m.Mail + "," + strconv.Itoa(m.Age) + ">"
+}
+
+func main() {
+	con, er := sql.Open("sqlite3", "data.sqlite3")
+	if er != nil {
+		panic(er)
+	}
+	defer con.Close()
+
+	nm := hello.Input("name")
+	ml := hello.Input("mail")
+	age := hello.Input("age")
+	ag, _ := strconv.Atoi(age)
+
+	qry := "insert into mydata (name, mail, age) values (?,?,?)"
+	con.Exec(qry, nm, ml, ag)
+	showRecord(con)
+
+}
+
+func showRecord(con *sql.DB) {
+	qry := "select * from mydata"
+	rs, _ := con.Query(qry)
+	for rs.Next() {
+		fmt.Println(mydatafmRws(rs).Str())
+	}
+}
+
+func mydatafmRws(rs *sql.Rows) *Mydata {
+	var md Mydata
+	er := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
+	if er != nil {
+		panic(er)
+	}
+	return &md
+}
+
+func mydatafmRw(rs *sql.Row) *Mydata {
+	var md Mydata
+	er := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
+	if er != nil {
+		panic(er)
+	}
+	return &md
+}
