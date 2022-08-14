d2f1d8a8ffdb8a472caf03b7b83581fd1cdb69d0
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-14 17:17:21 +0900

    list6-2,list6-3まで学習完了

--- go-web/web.go
+++ go-web/web.go
@@ -4,5 +4,8 @@ import "net/http"
 
 func main() {
 	// list6-1 NotFoundサーバを動かす
-	http.ListenAndServe(":8080", http.NotFoundHandler())
+	// http.ListenAndServe(":8080", http.NotFoundHandler())
+
+	// list6-2 ファイルサーバを動かす
+	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
 }
