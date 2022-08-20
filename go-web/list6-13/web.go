f86960a1ae06444ae496261860250cd15172e741
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-20 18:53:25 +0900

    list6-13まで学習完了

---
+++ go-web/list6-13/web.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type Temps struct {
+	notemp *template.Template
+	indx   *template.Template
+	helo   *template.Template
+}
+
+func notemp() *template.Template {
+	src := "<html><body><h1>NO TEMPATE.</h1></body></html>"
+	tmp, _ := template.New("index").Parse(src)
+	return tmp
+}
+
+func setupTemp() *Temps {
+	temps := new(Temps)
+
+	temps.notemp = notemp()
+
+	// set index template
+	indx, er := template.ParseFiles("../templates/index.html")
+	if er != nil {
+		indx = temps.notemp
+	}
+	temps.indx = indx
+
+	// set hello template
+	helo, er := template.ParseFiles("../templates/hello.html")
+	if er != nil {
+		helo = temps.notemp
+	}
+	temps.helo = helo
+
+	return temps
+}
+
+func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
+	er := tmp.Execute(w, nil)
+	if er != nil {
+		log.Fatal(er)
+	}
+}
+
+var flg bool = true
+
+func hello(w http.ResponseWriter, rq *http.Request,
+	tmp *template.Template) {
+
+	item := struct {
+		Flg      bool
+		Title    string
+		Message  string
+		JMessage string
+	}{
+		Flg:      flg,
+		Title:    "Send values",
+		Message:  "This is Sample message.<br>これはサンプルです。",
+		JMessage: "これはサンプルです。",
+	}
+
+	er := tmp.Execute(w, item)
+	if er != nil {
+		log.Fatal(er)
+	}
+	flg = !flg
+}
+
+func main() {
+	temps := setupTemp()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
+		index(w, rq, temps.indx)
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, rq *http.Request) {
+		hello(w, rq, temps.helo)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
