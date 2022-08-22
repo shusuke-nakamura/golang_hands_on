ef02950d03ef2ff86a3d8691a69619e3a88cbed9
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-08-22 12:43:28 +0900

    Insert文の実行

---
+++ youtube-bord/sqls/insert_users.sql
@@ -0,0 +1,2 @@
+insert into "users" values(1, null, null, null, 'taro@yamada.jp', 'taro', 'yamada', 'taro''s account.');
+insert into "users" values(2, null, null, null, 'hanako@flower.com', 'hanako', 'flower', 'hanako''s account.');
